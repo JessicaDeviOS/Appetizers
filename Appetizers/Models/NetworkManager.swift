ba78a43e165e01163c7a3edc9accfa72000ff579
Jessica Mallian <jlmallian@gmail.com>
2022-08-10 19:25:28 -0500

    have network call for image

--- Appetizers/Models/NetworkManager.swift
+++ Appetizers/Models/NetworkManager.swift
@@ -5,12 +5,13 @@
 //  Created by Jessica Mallian on 8/8/22.
 //
 
-import Foundation
+import UIKit
 
 final class NetworkManager {
     static let shared = NetworkManager()
     static let baseURL = "https://seanallen-course-backend.herokuapp.com/swiftui-fundamentals/"
     private let appetizerURL = baseURL + "appetizers"
+    private let cache = NSCache<NSString, UIImage>()
     
     private init() {}
     
@@ -42,5 +43,28 @@ final class NetworkManager {
             }
         }
         task.resume()
+    }
+    
+    func downloadImage(fromURLString urlString: String, completed: @escaping (UIImage?) -> Void) {
+        let cacheKey = NSString(string: urlString)
+        if let image = cache.object(forKey: cacheKey) {
+            completed(image)
+            return
+        }
+        
+        guard let url = URL(string: urlString) else {
+            completed(nil)
+            return
+        }
+        
+        let task = URLSession.shared.dataTask(with: URLRequest(url: url)) { data, response, error in
+            guard let data = data, let image = UIImage(data: data) else {
+                completed(nil)
+                return
+            }
+            self.cache.setObject(image, forKey: cacheKey)
+            completed(image)
+        }
+        task.resume() 
     }
 }
